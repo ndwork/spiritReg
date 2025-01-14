8746951813e91d61353b0d4fda6a822ab658f9fa
Nicholas Dwork <ndwork@gmail.com>
2025-01-14 08:27:38 -0700

    Changed undersampling pattern in run_reconSpiritReg.

--- run_reconSpiritReg.m
+++ run_reconSpiritReg.m
@@ -4,7 +4,7 @@ function run_reconSpiritReg( datacases )
   showScale = 3;
   verbose = true;
   wSize = 5;
-  sACR = 31;
+  sACR = 19;
   sampleFraction = 0.3;
 
   if nargin < 1
@@ -31,17 +31,19 @@ function run_reconSpiritReg( datacases )
     acrMask = padData( ones( sACR, sACR ), sImg );
     %sampleMask = mri_makeSampleMask( sImg, nSamples, 'maskType', 'VDPD', 'startMask', acrMask );
     sampleMask = ones( size( kData, [1 2] ) );
-    sampleMask(:,1:3:end) = 0;
-    sampleMask(:,2:3:end) = 0;
+    sampleMask(:,1:2:end) = 0;
+    sampleMask(1:2:end,:) = 0;
     sampleMask = sampleMask | acrMask;
     kSamples = bsxfun( @times, kData, sampleMask );
 
     %sMaps = mri_makeSensitivityMaps( kSamples );
 
-    img = mri_reconSpiritReg( kSamples, sACR, wSize, 'lambda', 1d-3, 'sMaps', sMaps );
+    imgMBR = mri_reconModelBased( kData, 'sMaps', sMaps );
+    imgSpiritReg = mri_reconSpiritReg( kSamples, sACR, wSize, 'lambda', 1, 'sMaps', sMaps );
     %img = mri_reconSpirit( kSamples, sACR, wSize, 'epsilon', 1 );
 
-    figure;  imshowscale( abs( img ) , 3 );
+    figure;  imshowscale( abs( imgMBR ) , 3 );
+    figure;  imshowscale( abs( imgSpiritReg ) , 3 );
     disp( 'I got here' );
   end
 end
